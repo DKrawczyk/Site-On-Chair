0e2cf07eb59486bd55b63ed971bec7f3b50afc9e
DKrawczyk <dawidosss1998@gmail.com>
2019-03-07 12:50:12 +0100

    changes

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// var list = document.querySelectorAll('.head > ul > li');
+// console.log(list);
+
+// list.forEach(function (el) {
+//     el.addEventListener('mouseover', function (){
+//         if (this.firstElementChild) {
+//             this.firstElementChild.style.display = 'inline-block';
+//         }
+//     });
+
+//     el.addEventListener('mouseout', function(){
+//         if(this.firstElementChild) {
+//             this.firstElementChild.style.display = 'none';
+//         }
+//     });
+// });
+
